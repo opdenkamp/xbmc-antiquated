opdenkamp/xbmc-antiquated

commit f2d2a2ba89c2f1070e70f27de08bb23a6c38fae9

[tvheadend] Fix Windows build under VS2008

xbmc/pvrclients/tvheadend/HTSPData.cpp
@@ -187,7 +187,7 @@ PVR_ERROR cHTSPData::RequestChannelList(PVRHANDLE handle, int radio)
     tag.stream_url    = "";
     tag.bouquet       = 0;
 
-    if(((bool)radio) == tag.radio)
+    if(radio && tag.radio)
     {
       PVR->TransferChannelEntry(handle, &tag);
     }

project/VS2008Express/XBMC for Windows.sln
@@ -124,6 +124,9 @@ EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "pvrclient_mptv", "..\..\xbmc\pvrclients\MediaPortal\project\VS2008Express\XBMC_MPTV.vcproj", "{74C9642E-1988-48DC-8404-11717C355378}"
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "pvrclient_hts", "..\..\xbmc\pvrclients\tvheadend\project\VS2008Express\XBMC_hts.vcproj", "{2F4A2296-3E00-4881-B6BC-1B7ADAC3CBF4}"
+	ProjectSection(ProjectDependencies) = postProject
+		{00700E12-A63B-4E54-B962-4011A90584BD} = {00700E12-A63B-4E54-B962-4011A90584BD}
+	EndProjectSection
 EndProject
 Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "libdts_dll", "..\..\xbmc\cores\dvdplayer\Codecs\libdts\vc++\libdts.vcproj", "{F93133AB-EB51-4955-AEDF-639632891042}"
 EndProject
@@ -528,6 +531,14 @@ Global
 		{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}.Release (DirectX)|Win32.Build.0 = Release|Win32
 		{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}.Release (OpenGL)|Win32.ActiveCfg = Release|Win32
 		{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}.Release (OpenGL)|Win32.Build.0 = Release|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (DirectX)|Win32.ActiveCfg = Debug|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (DirectX)|Win32.Build.0 = Debug|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (OpenGL)|Win32.ActiveCfg = Debug|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (OpenGL)|Win32.Build.0 = Debug|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (DirectX)|Win32.ActiveCfg = Release|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (DirectX)|Win32.Build.0 = Release|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (OpenGL)|Win32.ActiveCfg = Release|Win32
+		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (OpenGL)|Win32.Build.0 = Release|Win32
 		{838CDE27-AFDF-449F-9981-A78903C9C71E}.Debug (DirectX)|Win32.ActiveCfg = Debug|Win32
 		{838CDE27-AFDF-449F-9981-A78903C9C71E}.Debug (DirectX)|Win32.Build.0 = Debug|Win32
 		{838CDE27-AFDF-449F-9981-A78903C9C71E}.Debug (OpenGL)|Win32.ActiveCfg = Debug|Win32
@@ -736,14 +747,6 @@ Global
 		{1E2FB608-3DD2-4021-A598-90008FA6DE85}.Release (DirectX)|Win32.Build.0 = Release|Win32
 		{1E2FB608-3DD2-4021-A598-90008FA6DE85}.Release (OpenGL)|Win32.ActiveCfg = Release|Win32
 		{1E2FB608-3DD2-4021-A598-90008FA6DE85}.Release (OpenGL)|Win32.Build.0 = Release|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (DirectX)|Win32.ActiveCfg = Debug|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (DirectX)|Win32.Build.0 = Debug|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (OpenGL)|Win32.ActiveCfg = Debug|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Debug (OpenGL)|Win32.Build.0 = Debug|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (DirectX)|Win32.ActiveCfg = Release|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (DirectX)|Win32.Build.0 = Release|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (OpenGL)|Win32.ActiveCfg = Release|Win32
-		{6D8C91F8-992F-4C83-9DE3-485D64EF8420}.Release (OpenGL)|Win32.Build.0 = Release|Win32
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE

xbmc/lib/libhts/Win32/include/stdint.h
@@ -210,7 +210,7 @@ typedef uint64_t  uintmax_t;
 #define UINT8_C(val)  val##ui8
 #define UINT16_C(val) val##ui16
 #define UINT32_C(val) val##ui32
-//#define UINT64_C(val) val##ui64
+#define UINT64_C(val) val##ui64
 
 // 7.18.4.2 Macros for greatest-width integer constants
 #define INTMAX_C   INT64_C

xbmc/pvrclients/tvheadend/HTSPSession.cpp
@@ -24,7 +24,9 @@
 #ifdef _MSC_VER
 #include <winsock2.h>
 #define SHUT_RDWR SD_BOTH
+#ifndef ETIMEDOUT
 #define ETIMEDOUT WSAETIMEDOUT
+#endif
 #else
 #include <netinet/in.h>
 #include <netinet/tcp.h>

xbmc/pvrclients/tvheadend/project/VS2008Express/XBMC_hts.vcproj
@@ -40,13 +40,14 @@
 			<Tool
 				Name="VCCLCompilerTool"
 				Optimization="0"
-				AdditionalIncludeDirectories="..\..\..\..\cores\dvdplayer\Codecs\ffmpeg;..\..\..\..\cores\dvdplayer\Codecs\ffmpeg\include;..\..\;..\..\libhts\win32\include;..\..\pthread_win32;..\..\windows;..\..\..\..\addons\include;..\..\..\..\cores\dvdplayer\DVDDemuxers;..\..\..\..\xbmc\lib"
+				AdditionalIncludeDirectories="..\..\..\..\cores\dvdplayer\Codecs\ffmpeg;..\..\..\..\cores\dvdplayer\Codecs\ffmpeg\include;..\..\;..\..\pthread_win32;..\..\windows;..\..\..\..\addons\include;..\..\..\..\cores\dvdplayer\DVDDemuxers;..\..\..\..\..\xbmc\lib;..\..\..\..\..\xbmc\lib\libhts\Win32\include"
 				PreprocessorDefinitions="_WIN32;WIN32;_WINDOWS;_WIN32PC;_USE_32BIT_TIME_T;_CRT_SECURE_NO_WARNINGS;_SCL_SECURE_NO_WARNINGS;_WINSOCKAPI_;USE_DEMUX"
 				MinimalRebuild="true"
 				BasicRuntimeChecks="3"
 				RuntimeLibrary="3"
 				WarningLevel="3"
 				DebugInformationFormat="4"
+				DisableSpecificWarnings="4996"
 			/>
 			<Tool
 				Name="VCManagedResourceCompilerTool"
@@ -61,6 +62,7 @@
 				Name="VCLinkerTool"
 				AdditionalDependencies="ws2_32.lib ..\..\pthread_win32\pthreadVC2.lib"
 				OutputFile="../../../../../addons/pvr.hts/XBMC_Tvheadend_win32.pvr"
+				IgnoreDefaultLibraryNames="libcmtd"
 				GenerateDebugInformation="true"
 				TargetMachine="1"
 			/>
@@ -113,12 +115,13 @@
 				Name="VCCLCompilerTool"
 				Optimization="2"
 				EnableIntrinsicFunctions="true"
-				AdditionalIncludeDirectories="..\..\..\..\cores\dvdplayer\Codecs\ffmpeg;..\..\..\..\cores\dvdplayer\Codecs\ffmpeg\include;..\..\;..\..\libhts\win32\include;..\..\pthread_win32;..\..\windows;..\..\..\..\addons\include;..\..\..\..\cores\dvdplayer\DVDDemuxers"
+				AdditionalIncludeDirectories="..\..\..\..\cores\dvdplayer\Codecs\ffmpeg;..\..\..\..\cores\dvdplayer\Codecs\ffmpeg\include;..\..\;..\..\pthread_win32;..\..\windows;..\..\..\..\addons\include;..\..\..\..\cores\dvdplayer\DVDDemuxers;..\..\..\..\..\xbmc\lib;..\..\..\..\..\xbmc\lib\libhts\Win32\include"
 				PreprocessorDefinitions="_WIN32;WIN32;_WINDOWS;_WIN32PC;_USE_32BIT_TIME_T;_CRT_SECURE_NO_WARNINGS;_SCL_SECURE_NO_WARNINGS;_WINSOCKAPI_;USE_DEMUX"
 				RuntimeLibrary="2"
 				EnableFunctionLevelLinking="true"
 				WarningLevel="3"
 				DebugInformationFormat="3"
+				DisableSpecificWarnings="4996"
 			/>
 			<Tool
 				Name="VCManagedResourceCompilerTool"
@@ -133,6 +136,7 @@
 				Name="VCLinkerTool"
 				AdditionalDependencies="ws2_32.lib ..\..\pthread_win32\pthreadVC2.lib"
 				OutputFile="../../../../../addons/pvr.hts/XBMC_Tvheadend_win32.pvr"
+				IgnoreDefaultLibraryNames="libcmt"
 				GenerateDebugInformation="true"
 				OptimizeReferences="2"
 				EnableCOMDATFolding="2"

xbmc/pvrclients/tvheadend/windows/os_windows.h
@@ -27,7 +27,7 @@
 # define __USE_FILE_OFFSET64	1
 #endif
 
-typedef int ssize_t;
+//typedef int ssize_t;
 typedef int mode_t;
 typedef int bool_t;
 typedef __int8 int8_t;

commit 061c49def8bce549e505cb47202fd98a77e1f46b

tvheadend addon: commit f2d2a2ba89c2f1070e70f27de08bb23a6c38fae9 broke the tvheadend addon. noticed it before but forgot to correct after the cherry-pick. thanks Kenodai for telling me.

xbmc/pvrclients/tvheadend/HTSPData.cpp
@@ -187,7 +187,7 @@ PVR_ERROR cHTSPData::RequestChannelList(PVRHANDLE handle, int radio)
     tag.stream_url    = "";
     tag.bouquet       = 0;
 
-    if(radio && tag.radio)
+    if(((bool)radio) == tag.radio)
     {
       PVR->TransferChannelEntry(handle, &tag);
     }